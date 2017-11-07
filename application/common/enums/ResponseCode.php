167aa816092edf712af4cc4e33a1d83761bc3d09
kuasidufa <1878927986@qq.com>
2017-11-07 18:24:04 +0800

    add

---
+++ application/common/enums/ResponseCode.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\common\enums;
+/**
+ * User: liaoyizhong
+ * Date: 2017/11/7/007
+ * Time: 16:36
+ */
+
+class ResponseCode
+{
+    const SUCCESS = 0;
+
+    const PARAMS_MISS = 1001;
+
+    const LOGIC_ERROR = 2001;
+
+    const UNKNOW_ERROR = 3001;
+}
